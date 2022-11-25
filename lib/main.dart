072a9c90d3fcce592fecb7a4cd80458fa4b6abd3
walyazji <waseemyazji3@gmail.com>
2022-11-25 22:22:47 +0300

    Flutter UI 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,200 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyHomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Demo',
+//       themeMode: ThemeMode.light,
+//       theme: ThemeData(primarySwatch: Colors.teal, canvasColor: Colors.white),
+//       darkTheme:
+//           ThemeData(primarySwatch: Colors.teal, canvasColor: Colors.black),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  bool js = false;
+  bool cSharp = false;
+  bool py = false;
+  bool sw = false;
+  // Color w = Colors.white;
+  Color b = Colors.black;
+  String get txt {
+    String str = 'You Selected \n';
+    if (js == true) {
+      str += 'Javascript \n';
+    }
+    if (cSharp == true) {
+      str += 'C# \n';
+    }
+    if (py == true) {
+      str += 'Python \n';
+    }
+    return str;
   }
 
+  var tm = ThemeMode.light;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Demo',
+      themeMode: tm,
+      theme: ThemeData(primarySwatch: Colors.teal, canvasColor: Colors.white),
+      darkTheme:
+          ThemeData(primarySwatch: Colors.teal, canvasColor: Colors.black),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Demo App'),
+        ),
+        // body: buildCheckboxListTile(context),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(40.0),
+                child: Text(
+                  'Light',
+                  style: TextStyle(
+                      color: b,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+              Switch(
+                activeColor: Colors.black,
+                inactiveThumbColor: Colors.white,
+                value: sw,
+                onChanged: (value) {
+                  setState(() {
+                    sw = value;
+
+                    if (sw == false) {
+                      tm = ThemeMode.light;
+                      b = Colors.black;
+                    } else {
+                      tm = ThemeMode.dark;
+                      b = Colors.white;
+                    }
+                  });
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.all(40.0),
+                child: Text(
+                  'Dark',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: b,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Padding buildCheckboxListTile(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            'Select All The Programming Languages You Knows : ',
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Theme.of(context).primaryColor),
+          ),
+          CheckboxListTile(
+              title: const Text('Javascript'),
+              subtitle: const Text('Language No.1'),
+              value: js,
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged: (val) {
+                setState(() {
+                  js = val as bool;
+                });
+              }),
+          CheckboxListTile(
+              title: const Text('C#'),
+              subtitle: const Text('Language No.2'),
+              value: cSharp,
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged: (val) {
+                setState(() {
+                  cSharp = val as bool;
+                });
+              }),
+          CheckboxListTile(
+              title: const Text('Python'),
+              subtitle: const Text('Language No.3'),
+              value: py,
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged: (val) {
+                setState(() {
+                  py = val as bool;
+                });
+              }),
+          const Padding(padding: EdgeInsets.only(top: 5.0)),
+          ElevatedButton(
+              onPressed: (() {
+                showDialog(
+                  context: context,
+                  builder: (c) => AlertDialog(
+                    // showDialog(
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text(
+                          'Thank you for choose it',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const Padding(padding: EdgeInsets.only(top: 10)),
+                        Text(
+                          txt,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).primaryColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [Text('Submit'), Icon(Icons.ads_click)],
+              ))
+        ],
+      ),
     );
   }
 }
